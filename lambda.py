cd06cb3597aaa99987d6ad76b29a60a7de926378
Prosper Chigozie <prospachigoz@gmail.com>
2025-01-17 14:14:41 +0100

    Delete lambda.py

--- lambda.py
+++
@@ -1,7 +0,0 @@
-#A LAMBDA FUNCTION IS A SMALL FUNCTION, IT IS ANONYMOUS
-#LAMBDA CAN HAVE MANY ARGUMENTS, BUT ONE EXPRESSION
-
-def my_lambda(x):
-   x = lambda a : a * 5
-   return (x(5))
-my_lambda(5)
