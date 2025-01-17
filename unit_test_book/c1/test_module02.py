2cd804074165f53a7061461823f9013828db18ae
prospa <prospachigoz@gmail.com>
2025-01-17 13:58:05 +0100

    Learning unit testig in python

---
+++ unit_test_book/c1/test_module02.py
@@ -0,0 +1,17 @@
+"""
+SAMPLE DOCTEST TEST MODULE...test_module02
+"""
+def employee(name, age):
+    """
+    This function prints the name and age of employee
+    >>> employee("prosper", 20)
+    prosper 20
+    """
+    print(f"{name} {age}")
+def sub (a,b):
+   """
+   This function subtracts b from a
+   >>> sub(20,10)
+   10
+   """
+   return a - b 
